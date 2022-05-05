18f02abde834ae3bebbe33dab5fb153224f77259
Godfrey Bongomin <bongomin@uw.edu>
2022-05-05 10:25:50 -0700

    Added a delete method function in the api route

--- Routes/apiRoute.js
+++ Routes/apiRoute.js
@@ -1,8 +1,11 @@
 const apiRoute = require("express").Router();
 const path = require("path");
+const uuidv1 = require("uuid/v1");
 
 // apiRoute GET method
 apiRoute.get("/api/notes", (req, res) => {});
 
 // apiRoute POST method
 apiRoute.post("/api/notes", (req, res) => {});
+
+apiRoute.delete("/api/notets/:uuidv1", (req, res) => {});
